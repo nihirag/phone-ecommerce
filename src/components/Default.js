8a95d7d52dfa7ba88330c12a5581789e12b2b429
Nihir Agarwal <f20180701@pilani.bits-pilani.ac.in>
2020-05-13 16:49:07 +0530

    initial commit

---
+++ src/components/Default.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Default extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello From Default</h3>
+      </div>
+    );
+  }
+}
